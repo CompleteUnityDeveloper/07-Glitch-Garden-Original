92f6bd7964d7b0c21211907134f6c1a150b08a7b
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-26 18:19:16 +0000

    GG07 Alternative Music Manager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake() {
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't destory on load: " + name);
+	}
+	
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded (int level) {
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		Debug.Log ("Playing clip: " + thisLevelMusic);
+		
+		if (thisLevelMusic) { // If there's some music attached
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+}
