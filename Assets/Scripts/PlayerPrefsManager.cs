6f657310ce3af70df0e52117aeb1169217977561
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-27 16:42:05 +0000

    GG11 Our PlayerPrefsManager.cs

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_KEY = "level_unlocked_";
+
+	public static void SetMasterVolume (float volume) {
+		if (volume > 0f && volume < 1f) {
+			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
+		} else {
+			Debug.LogError ("Master volume out of range");
+		}
+	}
+	
+	public static float GetMasterVolume () {
+		return PlayerPrefs.GetFloat (MASTER_VOLUME_KEY);
+	}
+	
+	
+	public static void UnlockLevel (int level) {
+		if (level <= Application.levelCount - 1) {
+			PlayerPrefs.SetInt (LEVEL_KEY + level.ToString(), 1); // Use 1 for true
+		} else {
+			Debug.LogError ("Trying to unlock level not in build order");
+		}
+	}
+	
+	public static bool IsLevelUnlocked (int level) {
+		int levelValue = PlayerPrefs.GetInt (LEVEL_KEY + level.ToString());
+		bool isLevelUnlocked = (levelValue == 1);		
+
+		if (level <= Application.levelCount - 1) {
+			return isLevelUnlocked;
+		} else {
+			Debug.LogError ("Trying to query level not in build order");
+			return false;
+		}
+	}
+	
+	
+	public static void SetDifficulty (float difficulty) {
+		if (difficulty >= 0f && difficulty <= 1f) {
+			PlayerPrefs.SetFloat (DIFFICULTY_KEY, difficulty);
+		} else {
+			Debug.LogError ("Difficulty out of range");
+		}
+	}
+	
+	public static float GetDifficulty () {
+		return PlayerPrefs.GetFloat (DIFFICULTY_KEY);
+	}
+}
