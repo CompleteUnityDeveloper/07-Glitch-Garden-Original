5b18fca14c7caac6ff289c324247230dc0708d8e
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-27 13:49:26 +0000

    GG09 Adding Fade Transitions

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+	public float fadeInTime;
+	
+	private Image fadePanel;
+	private Color currentColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad < fadeInTime) {
+			// Fade in
+			float alphaChange = Time.deltaTime / fadeInTime;
+			currentColor.a -= alphaChange;
+			fadePanel.color = currentColor;
+		} else {
+			gameObject.SetActive (false);
+		}
+	}
+}
