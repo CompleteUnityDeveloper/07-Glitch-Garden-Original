3614964c3c313a6fe6c4f910259dfd8eef12f941
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-08 13:09:00 +0000

    GG24 Moving Attackers From Script

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-1f, 1.5f)]
+	public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+}
