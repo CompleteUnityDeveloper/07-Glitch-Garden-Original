41f6fe31f91ccfe3939f2b7c631b9c9f0a942b81
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-08 23:57:32 +0000

    GG25 Collision Matrix In Script

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -8,11 +8,16 @@ public class Attacker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+		myRigidbody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter2D () {
+		Debug.Log (name + " trigger enter");
 	}
 }
---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D () {
+		Debug.Log (name + " trigger enter");
+	}
+}
