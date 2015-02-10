3fdfff5f3593fe509dc8adbbfd34adb35965071a
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-10 14:06:35 +0000

    GG29 Animating Defenders & Projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float speed, damage;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+}
