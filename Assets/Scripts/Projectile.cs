4e9fd688cf3862fa0a75f38033f44e74a75642fd
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-10 17:32:01 +0000

    GG32 Handling Projectile Damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,4 +15,13 @@ public class Projectile : MonoBehaviour {
 		transform.Translate (Vector3.right * speed * Time.deltaTime);
 	}
 
+	void OnTriggerEnter2D (Collider2D collider) {
+		Attacker attacker = collider.gameObject.GetComponent<Attacker>();
+		Health health = collider.gameObject.GetComponent<Health>();
+		
+		if (attacker && health) {
+			health.DealDamage (damage);
+			Destroy (gameObject);
+		}
+	}
 }
