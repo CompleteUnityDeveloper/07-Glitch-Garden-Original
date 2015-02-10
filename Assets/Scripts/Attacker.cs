230e77b5b530b003967a11b617edddd4f53bed41
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-10 11:00:07 +0000

    GG28 Using A Health Component

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,10 +6,18 @@ public class Attacker : MonoBehaviour {
 
 	private float currentSpeed;
 	private GameObject currentTarget;
+	private Animator animator;
+	
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+		if (!currentTarget) {
+			animator.SetBool ("isAttacking", false);
+		}
 	}
 	
 	void OnTriggerEnter2D () {
@@ -22,11 +30,15 @@ public class Attacker : MonoBehaviour {
 	
 	// Called from the animator at time of actual blow
 	public void  StrikeCurrentTarget (float damage) {
-		Debug.Log (name + " caused damage: " + damage);
+		if (currentTarget) {
+			Health health = currentTarget.GetComponent<Health>();
+			if (health) {
+				health.DealDamage (damage);
+			}
+		}
 	}
 	
 	public void Attack (GameObject obj) {
 		currentTarget = obj;
-		
 	}
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	
+	public float health = 100f;
+	
+	public void DealDamage (float damage) {
+		health -= damage;
+		if (health < 0) {
+			// Optionally trigger animation
+			DestoryObject ();
+		}
+	}
+	
+	public void DestoryObject () {
+		Destroy (gameObject);
+	}
+	
+}
