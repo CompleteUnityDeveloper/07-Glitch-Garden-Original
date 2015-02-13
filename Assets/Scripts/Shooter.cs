a27d97951737b1d50e86847fdadd8098eccd9d2b
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-13 18:40:40 +0000

    GG34 Creating When Needed

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
 
-	public GameObject projectile, projectileParent, gun;
+	public GameObject projectile, gun;
+	
+	private GameObject projectileParent;
+	
+	void Start () {
+		projectileParent = GameObject.Find ("Projectiles");
+		
+		if (!projectileParent) {
+			projectileParent = new GameObject("Projectiles");
+		}
+	}
 	
 	private void Fire () {
 		GameObject newProjectile = Instantiate (projectile) as GameObject;
