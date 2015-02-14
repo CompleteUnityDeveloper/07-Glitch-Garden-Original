b6b81fa5fafd2e335d24e46b24becdb6bec34931
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-14 09:00:09 +0000

    GG36 Enemy Spawning & Flow

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,6 +4,8 @@ using System.Collections;
 [RequireComponent (typeof (Rigidbody2D))]
 public class Attacker : MonoBehaviour {
 
+	[Tooltip ("Average number of seconds between appearances")]
+	public float seenEverySeconds;
 	private float currentSpeed;
 	private GameObject currentTarget;
 	private Animator animator;
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] attackerPrefabArray;
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject thisAttacker in attackerPrefabArray) {
+			if (isTimeToSpawn (thisAttacker)) {
+				Spawn (thisAttacker);
+			}
+		}	
+	}
+	
+	void Spawn (GameObject myGameObject) {
+		GameObject myAttacker = Instantiate (myGameObject) as GameObject;
+		myAttacker.transform.parent = transform;
+		myAttacker.transform.position = transform.position;
+	}
+	
+	bool isTimeToSpawn (GameObject attackerGameObject) {
+		Attacker attacker = attackerGameObject.GetComponent<Attacker>();
+		
+		float meanSpawnDelay = attacker.seenEverySeconds;
+		float spawnsPerSecond = 1 / meanSpawnDelay;
+		
+		if (Time.deltaTime > meanSpawnDelay) {
+			Debug.LogWarning ("Spwan rate capped by frame rate");
+		}
+		
+		float threshold = spawnsPerSecond * Time.deltaTime / 5;
+		
+		if (Random.value < threshold) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		return true;
+	}
+}
