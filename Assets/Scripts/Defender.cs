f086b36d68857fbbf3cba2ac690538206224838b
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-20 13:59:19 +0000

    GG38 Using Stars As Currency

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class Defender : MonoBehaviour {
 	
-	// Only being used as a tag for now!
+	private StarDisplay starDisplay;
+
+	void Start () {
+		starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+	}
 	
+	// Only being used as a tag for now!
+	public void AddStars (int amount) {
+		starDisplay.AddStars (amount);
+	}
 }
---
+++ Assets/StarDisplay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class StarDisplay : MonoBehaviour {
+
+	private Text starText;
+	private int stars = 0;
+
+	// Use this for initialization
+	void Start () {
+		starText = GetComponent <Text>();
+	}
+	
+	public void AddStars (int amount) {
+		stars += amount;
+		UpdateDisplay();
+	}
+	
+	public void UseStars (int amount) {
+		stars -= amount;
+		UpdateDisplay();
+	}
+	
+	private void UpdateDisplay () {
+		starText.text =stars.ToString();
+	}
+	
+}
