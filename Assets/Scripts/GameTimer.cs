4afded4a016e0af7b58102558f382b2747c21fb8
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-23 15:58:46 +0000

    Game Timer - before Camtasia crash

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+
+	public float levelSeconds = 100;
+	
+	private Slider slider;
+	private LevelManager levelManager;
+	private AudioClip audioClip;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider>();
+		audioClip = GetComponent<AudioClip>();
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = Time.timeSinceLevelLoad / levelSeconds;
+		
+		if (Time.timeSinceLevelLoad >= levelSeconds) { // Level won
+			audio.Play();
+			levelManager.LoadNextLevel();
+		}
+	}
+}
