7774f402c838a9131743f71ef41cd8fb148c2d42
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-23 17:11:43 +0000

    GG41 UI Slider Level Timer

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -5,25 +5,44 @@ using System.Collections;
 public class GameTimer : MonoBehaviour {
 
 	public float levelSeconds = 100;
-	
+
 	private Slider slider;
+	private AudioSource audioSource;
+	private bool isEndOfLevel = false;
 	private LevelManager levelManager;
-	private AudioClip audioClip;
+	private GameObject winLabel;
 
 	// Use this for initialization
 	void Start () {
 		slider = GetComponent<Slider>();
-		audioClip = GetComponent<AudioClip>();
+		audioSource = GetComponent<AudioSource>();
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		FindYouWin ();
+		winLabel.SetActive(false);
+	}
+
+	void FindYouWin ()
+	{
+		winLabel = GameObject.Find ("You Win");
+		if (!winLabel) {
+			Debug.LogWarning ("Please create You Win object");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		slider.value = Time.timeSinceLevelLoad / levelSeconds;
 		
-		if (Time.timeSinceLevelLoad >= levelSeconds) { // Level won
-			audio.Play();
-			levelManager.LoadNextLevel();
+		bool timeIsUp = (Time.timeSinceLevelLoad >= levelSeconds);
+		if (timeIsUp && !isEndOfLevel) {
+			audioSource.Play ();
+			winLabel.SetActive(true);
+			Invoke ("LoadNextLevel", audioSource.clip.length);
+			isEndOfLevel = true;
 		}
+	}
+	
+	void LoadNextLevel () {
+		levelManager.LoadNextLevel();
 	}
 }
