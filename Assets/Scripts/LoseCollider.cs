bb245e427a6fdd68e9824a77d6cae3418ae7a994
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-02-23 14:06:50 +0000

    GG40 Handle Lose Condition

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	void OnTriggerEnter2D () {
+		levelManager.LoadLevel ("03b Lose");
+	}
+}
