5ee2c9cdddde03f8155d8067f18f3ba30d12f7da
Rick <rickinaction@gmail.com>
2018-10-13 18:21:39 +1000

    Mushed around with music manager.

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -11,45 +11,59 @@ public class PlayerPrefsManager : MonoBehaviour {
     {
 		if (volume >= 0f && volume <= 1f) {
 			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
-		} else {
+		}
+        else
+        {
 			Debug.LogError ("Master volume out of range");
 		}
 	}
 	
-	public static float GetMasterVolume () {
+	public static float GetMasterVolume()
+    {
 		return PlayerPrefs.GetFloat (MASTER_VOLUME_KEY);
 	}
 	
-	
-	public static void UnlockLevel (int level) {
-		if (level <= Application.levelCount - 1) {
+	public static void UnlockLevel (int level)
+    {
+		if (level <= Application.levelCount - 1)
+        {
 			PlayerPrefs.SetInt (LEVEL_KEY + level.ToString(), 1); // Use 1 for true
-		} else {
+		}
+        else
+        {
 			Debug.LogError ("Trying to unlock level not in build order");
 		}
 	}
 	
-	public static bool IsLevelUnlocked (int level) {
+	public static bool IsLevelUnlocked (int level)
+    {
 		int levelValue = PlayerPrefs.GetInt (LEVEL_KEY + level.ToString());
 		bool isLevelUnlocked = (levelValue == 1);		
 
-		if (level <= Application.levelCount - 1) {
+		if (level <= Application.levelCount - 1)
+        {
 			return isLevelUnlocked;
-		} else {
+		}
+        else
+        {
 			Debug.LogError ("Trying to query level not in build order");
 			return false;
 		}
 	}
 	
-	public static void SetDifficulty (float difficulty) {
+	public static void SetDifficulty (float difficulty)
+    {
 		if (difficulty >= 1f && difficulty <= 3f) {
 			PlayerPrefs.SetFloat (DIFFICULTY_KEY, difficulty);
-		} else {
+		}
+        else
+        {
 			Debug.LogError ("Difficulty out of range");
 		}
 	}
 	
-	public static float GetDifficulty () {
+	public static float GetDifficulty()
+    {
 		return PlayerPrefs.GetFloat (DIFFICULTY_KEY);
 	}
 }
